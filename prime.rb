25ad84c4d9e65e4822cacf085e28ef95c49ce13b
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 00:59:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(int)
   if int.abs % 2 == 0 && int.abs != 2 
     return false 
   end 
-  num =* (2..Math.sqrt(int.abs) + 1)
+  num =* (2..Math.sqrt(int.abs))
   num.each do |i|
     if int % i == 0 && i < int
       false 
